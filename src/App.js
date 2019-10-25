42fa45de56710ea41d4cdf8cb28b51801c781ffd
Leandro Kimura <leandrokk@jmalucelliti.com.br>
2019-10-25 08:50:08 -0300

    Add project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ToastAnimated, { showToast } from "./ui-lib/toast";
 
-function App() {
+export default function App() {
+  const handleClick = () =>
+    showToast({ type: "success", message: "Mensagem de sucesso" });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastAnimated />
+      <button onClick={handleClick}>Mostrar toast de sucesso</button>
+    </>
   );
 }
-
-export default App;
